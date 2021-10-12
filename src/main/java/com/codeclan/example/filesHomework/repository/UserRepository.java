6e316d3f5fd5099d744a22f6c4091442b40841a3
DSoutter <duncan.soutter@gmail.com>
2021-10-12 17:54:01 +0100

    added repositories

--- src/main/java/com/codeclan/example/filesHomework/repository/UserRepository.java
+++ src/main/java/com/codeclan/example/filesHomework/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.codeclan.example.filesHomework.repository;
 
 import com.codeclan.example.filesHomework.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
